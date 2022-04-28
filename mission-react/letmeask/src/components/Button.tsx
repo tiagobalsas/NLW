1970051a2e52c2d52d1c8140a36d82cf6747adaa
Tiago Santos <tiago881@gmail.com>
2022-04-28 16:13:29 -0300

    useState example

--- mission-react/letmeask/src/components/Button.tsx
+++ mission-react/letmeask/src/components/Button.tsx
@@ -1,9 +1,13 @@
-type ButtonProps = {
-  text?: string
-}
+import { useState } from 'react';
 
-export function Button(props: ButtonProps) {
-  return (
-    <button>{props.text || "Default"}</button>
-  )
+export function Button() {
+  // let counter = 0;
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter + 1);
+    console.log(counter);
+  }
+
+  return <button onClick={increment}>{counter}</button>;
 }
