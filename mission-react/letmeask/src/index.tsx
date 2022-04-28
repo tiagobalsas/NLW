a9bd5d875e7ff7ab717c502978d5aa7604139e41
Tiago Santos <tiago881@gmail.com>
2022-04-28 17:12:13 -0300

    Create and conect database firebase

--- mission-react/letmeask/src/index.tsx
+++ mission-react/letmeask/src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
+import './services/firebase';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
