2274e2a7c283bdf9de3c1b185de24607b4e08dbc
Tiago Santos <tiago881@gmail.com>
2022-04-29 15:27:14 -0300

    Apply style Enter Room Button

--- mission-react/letmeask/src/components/Button.tsx
+++ mission-react/letmeask/src/components/Button.tsx
@@ -1,13 +1,11 @@
-import { useState } from 'react';
+import {ButtonHTMLAttributes} from 'react'
 
-export function Button() {
-  // let counter = 0;
-  const [counter, setCounter] = useState(0);
+import '../styles/button.scss'
 
-  function increment() {
-    setCounter(counter + 1);
-    console.log(counter);
-  }
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
 
-  return <button onClick={increment}>{counter}</button>;
+export function Button(props: ButtonProps) {
+  return (
+    <button className='button' {...props} />
+  )
 }
--- mission-react/letmeask/src/pages/Home.tsx
+++ mission-react/letmeask/src/pages/Home.tsx
@@ -2,6 +2,9 @@ import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 
 import googleIconImg from '../assets/images/google-icon.svg';
+
+import { Button } from '../components/Button';
+
 import '../styles/auth.scss';
 
 export function Home() {
@@ -27,7 +30,7 @@ export function Home() {
           <div className='separator'>Ou entre em uma sala</div>
           <form action=''>
             <input type='text' placeholder='Digite o código da sala' />
-            <button type='submit'>Entrar na sala</button>
+            <Button type='submit'>Entrar na sala</Button>
           </form>
         </div>
       </main>
