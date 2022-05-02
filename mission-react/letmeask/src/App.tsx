b64b1182f59b265459d2549f1b17bbe6db35d1f5
Tiago Santos <tiago881@gmail.com>
2022-05-02 11:00:38 -0300

    Recovers the authentication state

--- mission-react/letmeask/src/App.tsx
+++ mission-react/letmeask/src/App.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import { auth, firebase } from './services/firebase';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
@@ -20,6 +20,24 @@ export const AuthContext = createContext({} as AuthContextTypes);
 
 function App() {
   const [user, setUser] = useState<User>();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        const { displayName, photoURL, uid } = user;
+
+        if (!displayName || !photoURL) {
+          throw new Error('Missing information from Google Account.');
+        }
+
+        setUser({
+          id: uid,
+          name: displayName,
+          avatar: photoURL,
+        });
+      }
+    });
+  }, []);
 
   async function signInWithGoogle() {
     const provaider = new firebase.auth.GoogleAuthProvider();
