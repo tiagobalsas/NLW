9db3b7f00514b6d5157abb199b166ed3db1932d0
Tiago Santos <tiago881@gmail.com>
2022-05-26 15:19:57 -0300

    Created server connection

--- mission-impulse/node_heat/src/app.ts
+++ mission-impulse/node_heat/src/app.ts
@@ -1,15 +1,25 @@
 import 'dotenv/config';
 import express from 'express';
 import http from 'http';
-import { Server } from 'socket.io';
+import cors from 'cors';
+import { Server, Socket } from 'socket.io';
 
 import { router } from './routes';
 
 const app = express();
+app.use(cors());
 
 const serverHttp = http.createServer(app);
 
-const io = new Server(serverHttp);
+const io = new Server(serverHttp, {
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`Usuário conectado no socket ${socket.id}`);
+});
 
 app.use(express.json());
 
