13e1ad79a21a7b402432ea407fc5637b7173da5f
Tiago Santos <tiago881@gmail.com>
2022-05-26 15:32:17 -0300

    [Fix]http server

--- mission-impulse/node_heat/src/app.ts
+++ mission-impulse/node_heat/src/app.ts
@@ -37,4 +37,4 @@ app.get('/signin/callback', (req, res) => {
   return res.json(code);
 });
 
-app.listen(4000, () => console.log('🚀 Server is running on PORT 4000'));
+serverHttp.listen(4000, () => console.log('🚀 Server is running on PORT 4000'));
