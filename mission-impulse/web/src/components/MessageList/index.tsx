e9a4c7602951aea84ec676bc205ea842e707d38b
Tiago Santos <tiago881@gmail.com>
2022-05-27 15:14:50 -0300

    Added news elements html

--- mission-impulse/web/src/components/MessageList/index.tsx
+++ mission-impulse/web/src/components/MessageList/index.tsx
@@ -1,3 +1,59 @@
-export function MessageList(params: type) {
-  return <h1>MessageList</h1>;
+import styles from './style.module.scss';
+
+import logoImg from '../../assets/logo.svg';
+
+export function MessageList() {
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt='DoWhile2021' />
+
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+            de todos os tempos, vamooo pra cima! 🔥🔥
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src='https://github.com/tiagobalsas.png'
+                alt='Tiago Santos'
+              />
+            </div>
+            <span>Tiago Santos</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+            de todos os tempos, vamooo pra cima! 🔥🔥
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src='https://github.com/tiagobalsas.png'
+                alt='Tiago Santos'
+              />
+            </div>
+            <span>Tiago Santos</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+            de todos os tempos, vamooo pra cima! 🔥🔥
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src='https://github.com/tiagobalsas.png'
+                alt='Tiago Santos'
+              />
+            </div>
+            <span>Tiago Santos</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
