bf325aff1161f1c3673c3aa9ba2f603cc53db67c
Tiago Santos <tiago881@gmail.com>
2022-05-30 15:18:57 -0300

    Create singOut function

--- mission-impulse/web/src/contexts/auth.tsx
+++ mission-impulse/web/src/contexts/auth.tsx
@@ -21,6 +21,7 @@ type UserTypes = {
 type AuthContextDataType = {
   user: UserTypes | null;
   signInUrl: string;
+  signOut: () => void;
 };
 
 export const AuthContext = createContext({} as AuthContextDataType);
@@ -43,6 +44,11 @@ export function AuthProvider(props: AuthProviderTypes) {
     localStorage.setItem('@dowhile:token', token);
 
     setUser(user);
+  }
+
+  function signOut() {
+    setUser(null);
+    localStorage.removeItem('@dowhile:token');
   }
 
   useEffect(() => {
@@ -70,7 +76,7 @@ export function AuthProvider(props: AuthProviderTypes) {
     }
   }, []);
   return (
-    <AuthContext.Provider value={{ signInUrl, user }}>
+    <AuthContext.Provider value={{ signInUrl, user, signOut }}>
       {props.children}
     </AuthContext.Provider>
   );
