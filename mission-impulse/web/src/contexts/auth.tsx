25cd0737babdf955e959effb82b0394a59d1de3c
Tiago Santos <tiago881@gmail.com>
2022-05-30 16:44:54 -0300

    Validate token

--- mission-impulse/web/src/contexts/auth.tsx
+++ mission-impulse/web/src/contexts/auth.tsx
@@ -43,6 +43,8 @@ export function AuthProvider(props: AuthProviderTypes) {
     const { token, user } = response.data;
     localStorage.setItem('@dowhile:token', token);
 
+    api.defaults.headers.common.authorization = `Bearer ${token}`;
+
     setUser(user);
   }
 
